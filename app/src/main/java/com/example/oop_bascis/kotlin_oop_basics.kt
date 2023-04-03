3603a1afe42bb9a696014aa8bdea92c820674cd0
1-MuhammadAli <ali.oraxtech@gmail.com>
2023-04-03 12:40:03 +0500

    Kotlin OOP Basics : Added -> scope And shadowing

--- app/src/main/java/com/example/oop_bascis/kotlin_oop_basics.kt
+++ app/src/main/java/com/example/oop_bascis/kotlin_oop_basics.kt
@@ -5,7 +5,7 @@ fun  main() {
     var ali = Person("Ahmed" , "Ali")
     var m = Person()
     var arslan = Person(lastName = "Arslan")
-
+    myFunction(a=5)
 }
 
 class Person(firstName: String = "M" , lastName: String = "Ali") {
@@ -14,3 +14,9 @@ class Person(firstName: String = "M" , lastName: String = "Ali") {
         println("Initialized a new Person object with firstName = $firstName and lastName = $lastName")
     }
 }
+
+fun myFunction(a:Int){
+    var a = 4
+    println("a is $a")
+}
+
