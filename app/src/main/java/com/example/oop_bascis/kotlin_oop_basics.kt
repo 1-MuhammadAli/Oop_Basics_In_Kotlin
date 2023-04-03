4b47c8a1d57cb304a526914f944099ade14ff4e6
1-MuhammadAli <ali.oraxtech@gmail.com>
2023-04-03 14:25:11 +0500

    Kotlin OOP Basics : Added -> member variables - functions and constructors

--- app/src/main/java/com/example/oop_bascis/kotlin_oop_basics.kt
+++ app/src/main/java/com/example/oop_bascis/kotlin_oop_basics.kt
@@ -2,16 +2,35 @@ package com.example.oop_bascis
 
 fun  main() {
    // Creating Instances of Classes
-    var ali = Person("Ahmed" , "Ali")
+    var ali = Person(firstName = "Ahmed" , lastName = "Ali" , age = 25)
     var m = Person()
+    ali.age = 24
+    println("Ali is ${ali.age} years old")
+    m.hobby="pigeon"
+    m.stateHobby()
     var arslan = Person(lastName = "Arslan")
     myFunction(a=5)
 }
 
 class Person(firstName: String = "M" , lastName: String = "Ali") {
 
+    var hobby : String ="Cricket"
+    var age : Int? = null
+    var firstName : String? = null
+
     init {
+        this.firstName = firstName
         println("Initialized a new Person object with firstName = $firstName and lastName = $lastName")
+    }
+
+    //Member secondary constructor
+    constructor(firstName: String , lastName: String , age: Int)
+            : this(firstName,lastName){
+                this.age = age
+        println("Initialized a new Person object with firstName = $firstName and lastName = $lastName and age $age")
+            }
+    fun stateHobby(){
+        println("$firstName\'s hobby is $hobby")
     }
 }
 
